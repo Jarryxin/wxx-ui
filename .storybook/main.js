37238d3f85e3b602229aa429a067e2fcd77279eb
Administrator <admin@example.com>
2020-08-16 10:48:31 +0800

    part1

---
+++ .storybook/main.js
@@ -0,0 +1,26 @@
+module.exports = {
+  stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-actions",
+    "@storybook/addon-viewport",
+    "@storybook/addon-knobs",
+    { name: "@storybook/addon-docs", options: { configureJSX: true } },
+    "@storybook/addon-a11y",
+    "@storybook/addon-storysource",
+    "@storybook/preset-create-react-app",
+  ],
+  webpackFinal: async (config) => {
+    config.module.rules.push({
+      test: /\.(ts|tsx)$/,
+      use: [
+        {
+          loader: require.resolve("react-docgen-typescript-loader"),
+        },
+      ],
+    });
+    config.resolve.extensions.push(".ts", ".tsx");
+    return config;
+  },
+};
