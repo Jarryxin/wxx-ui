a3f52b42863e54470962dec0cadea26efd228750
Administrator <admin@example.com>
2020-08-17 23:47:08 +0800

    part2

--- src/components/button/index.tsx
+++ src/components/button/index.tsx
@@ -1,15 +1,101 @@
-import React, { ButtonHTMLAttributes, PropsWithChildren } from "react";
-import styled from "styled-components";
+import React, { ButtonHTMLAttributes, PropsWithChildren, ReactNode, AnchorHTMLAttributes , useMemo } from "react";
+import { Text, Loading, StyledButton } from './style';
 
-const Mybutton = styled.button({
-  color: "red",
-});
+export type AppearancesTypes = keyof typeof APPEARANCES;
 
-interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {}
+type btnType =
+	| "primary"
+	| "primaryOutline"
+	| "secondary"
+	| "secondaryOutline"
+	| "tertiary"
+	| "outline"
+	| "inversePrimary"
+	| "inverseSecondary"
+	| "inverseOutline";
+
+type AppearancesObj = {
+	[key in btnType]: btnType;
+};
+
+export const APPEARANCES: AppearancesObj = {
+	primary: "primary",
+	primaryOutline: "primaryOutline",
+	secondary: "secondary",
+	secondaryOutline: "secondaryOutline",
+	tertiary: "tertiary",
+	outline: "outline",
+	inversePrimary: "inversePrimary",
+	inverseSecondary: "inverseSecondary",
+	inverseOutline: "inverseOutline",
+};
+
+export type SizesTypes = keyof typeof SIZES;
+type sizeType = "small" | "medium";
+type sizeObj = {
+	[key in sizeType]: sizeType;
+};
+export const SIZES: sizeObj = {
+	small: "small",
+	medium: "medium",
+};
+
+export interface CustormButtonProps {
+	/** 是否禁用 */
+	disabled?: boolean;
+	/** 是否加载中 */
+	isLoading?: boolean;
+	/** 是否是a标签 */
+	isLink?: boolean;
+	/** 是否替换加载中文本 */
+	loadingText?: ReactNode;
+	/** 按钮大小 */
+	size?: SizesTypes;
+	/** 按钮类型 */
+	appearance?: AppearancesTypes;
+	/** 无效点击 */
+	isUnclickable?: boolean;
+}
+
+export const btnPadding = {
+  medium: '14',
+  small: '8'
+}
+
+export type ButtonProps = CustormButtonProps &
+	AnchorHTMLAttributes<HTMLAnchorElement> &
+	ButtonHTMLAttributes<HTMLButtonElement>;
 
 function Button(props: PropsWithChildren<ButtonProps>) {
-  const { children, ...rest } = props;
-  return <Mybutton {...rest}>{children}</Mybutton>;
+  const { isLoading, loadingText, isLink, children } = props;
+  const buttonInner = (
+    <>
+      <Text>{children}</Text>
+      {isLoading && <Loading>{loadingText || "Loading..."}</Loading>}
+    </>
+  );
+  const btnType = useMemo(() => {
+    if (isLink) {
+      return "a";
+    }
+  }, [isLink]);
+  return (
+    <StyledButton as={btnType} {...props}>
+      {buttonInner}
+    </StyledButton>
+  );
+}
+
+Button.defaultProps = {
+  isLoading: false,
+	loadingText: null,
+	isLink: false,
+	appearance: APPEARANCES.tertiary,
+	isDisabled: false,
+	isUnclickable: false,
+	containsIcon: false,
+	size: SIZES.medium,
+	ButtonWrapper: undefined
 }
 
 export default Button;
