f6796a972268feb0e11fced24d067afe0ec6c898
Administrator <admin@example.com>
2020-08-19 09:12:58 +0800

    day5

--- .storybook/main.js
+++ .storybook/main.js
@@ -2,7 +2,6 @@ module.exports = {
   stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
   addons: [
     "@storybook/addon-links",
-    "@storybook/addon-essentials",
     "@storybook/addon-actions",
     "@storybook/addon-viewport",
     "@storybook/addon-knobs",
