606a0886b3e8afefce41553911dab77591ca3de4
Jarryxin <578676562@qq.com>
2020-08-21 00:52:12 +0000

    Deploying to gh-pages from  @ bce6241a27ec4279a3c9f536a68ba2856089d19f 🚀

---
+++ precache-manifest.fc4c96db2e7019cd4a29af7d4058ef8d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a89702613389fa9a62d275d71c6b2eae",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.e69e5a03434ea0fbf632.bundle.js"
+  },
+  {
+    "url": "runtime~main.e69e5a03434ea0fbf632.bundle.js"
+  },
+  {
+    "revision": "5c12deb26389178bc480",
+    "url": "static/css/main.4e7e4334.chunk.css"
+  },
+  {
+    "url": "vendors~main.e69e5a03434ea0fbf632.bundle.js"
+  },
+  {
+    "url": "vendors~main.e69e5a03434ea0fbf632.bundle.js.LICENSE.txt"
+  }
+]);
